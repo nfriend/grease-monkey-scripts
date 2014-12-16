ca32051054577e4658c16d971cb9a5bb9244c669
Nathan Friend <hello@nathanfriend.io>
2014-12-15 23:18:30 -0800

    added mass link opening with 1-9 + Enter

--- google-search-results-shortcuts.user.js
+++ google-search-results-shortcuts.user.js
@@ -11,18 +11,42 @@ var j = 74,
     k = 75,
     down = 40,
     up = 38,
-    body = document.getElementsByTagName('body')[0];
+    one = 49,
+    nine = 57,
+    enter = 13,
+    body = document.getElementsByTagName('body')[0],
+    linksToOpen = [];
 
-// make "J" and "K" synonymous with the up and down arrows
 body.addEventListener('keydown', function (e) {
+  
+  // make "J" and "K" synonymous with the up and down arrows
   if ((e.keyCode === j || e.keyCode === k) && !e.ctrlKey) {
     e.preventDefault();
     e.stopPropagation();
+    
     if (e.keyCode === j) {
       body.dispatchEvent(createKeyboardEvent(down));
     } else {
       body.dispatchEvent(createKeyboardEvent(up));
     }
+  }
+  
+  // make 1 - 9 open up the respective search result in a new tab, after pressing Enter
+  if (e.keyCode >= one && e.keyCode <= nine) {
+    e.preventDefault();
+    e.stopPropagation();
+  
+    var targetLink = document.getElementsByClassName('g')[e.keyCode - 49].getElementsByClassName('r')[0].getElementsByTagName('a')[0];
+    if (linksToOpen.indexOf(targetLink.href) === -1) {
+      linksToOpen.push(targetLink.href);
+    }
+  }
+  
+  if (e.keyCode === enter && linksToOpen.length > 0) {
+    for (var i = 0; i < linksToOpen.length; i++) {
+      window.open(linksToOpen[i], '_blank');
+    }
+    linksToOpen = [];
   }
 });
 
