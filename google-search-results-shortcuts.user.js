9112fa68e0fef85d29d762087acd586f315d3ada
Nathan Friend <hello@nathanfriend.io>
2014-12-20 10:57:17 -0600

    closes #2

--- google-search-results-shortcuts.user.js
+++ google-search-results-shortcuts.user.js
@@ -18,43 +18,47 @@ var j = 74,
     linksToOpen = [];
 
 body.addEventListener('keydown', function (e) {
-  
-  // make "K" and "J" synonymous with the up and down arrows
-  if ((e.keyCode === j || e.keyCode === k) && !e.ctrlKey) {
-    e.preventDefault();
-    e.stopPropagation();
-    
-    if (e.keyCode === j) {
-      body.dispatchEvent(createKeyboardEvent(down));
-    } else {
-      body.dispatchEvent(createKeyboardEvent(up));
+
+    // ignore keydowns if they're originating from an input element
+    if (e.target.nodeName.toLowerCase() === 'input')
+        return;
+
+    // make "K" and "J" synonymous with the up and down arrows
+    if ((e.keyCode === j || e.keyCode === k) && !e.ctrlKey) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (e.keyCode === j) {
+            body.dispatchEvent(createKeyboardEvent(down));
+        } else {
+            body.dispatchEvent(createKeyboardEvent(up));
+        }
     }
-  }
-  
-  // make 1 - 9 open up the respective search result in a new tab, after pressing Enter
-  if (e.keyCode >= one && e.keyCode <= nine) {
-    e.preventDefault();
-    e.stopPropagation();
-  
-    var targetLink = document.getElementsByClassName('g')[e.keyCode - 49].getElementsByTagName('a')[0];
-    if (linksToOpen.indexOf(targetLink.href) === -1) {
-      linksToOpen.push(targetLink.href);
+
+    // make 1 - 9 open up the respective search result in a new tab, after pressing Enter
+    if (e.keyCode >= one && e.keyCode <= nine) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var targetLink = document.getElementsByClassName('g')[e.keyCode - 49].getElementsByTagName('a')[0];
+        if (linksToOpen.indexOf(targetLink.href) === -1) {
+            linksToOpen.push(targetLink.href);
+        }
     }
-  }
-  
-  if (e.keyCode === enter && linksToOpen.length > 0) {
-    for (var i = linksToOpen.length - 1; i >= 0; i--) {
-      window.open(linksToOpen[i], '_blank');
+
+    if (e.keyCode === enter && linksToOpen.length > 0) {
+        for (var i = linksToOpen.length - 1; i >= 0; i--) {
+            window.open(linksToOpen[i], '_blank');
+        }
+        linksToOpen = [];
     }
-    linksToOpen = [];
-  }
 });
 
 function createKeyboardEvent(keyCode) {
-  return new KeyboardEvent('keydown', {
-    'view': window,
-    'bubbles': true,
-    'cancelable': true,
-    'keyCode': keyCode
-  });
+    return new KeyboardEvent('keydown', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': true,
+        'keyCode': keyCode
+    });
 }
